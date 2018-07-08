519e4345356d048567c4593e3c8ad0f37ef699ef
Raul Guerridos <raulguerridos@Rauls-MacBook-Pro.local>
2018-07-08 14:10:14 -0700

    This is actually lesson 9. Added AuthService.

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -8,8 +8,21 @@
 
 import Foundation
 
+//This remaps the type. So now CompletionHandler is of type Closure which is what (_ Success: Bool) is. 
+typealias CompletionHandler = (_ Succcess: Bool) -> ()
+
+//URL Constants
+let BASE_URl = "https://smackchatchattychat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
